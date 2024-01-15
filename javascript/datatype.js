21ca73bed921d98624b9b51efe67f4c6b8211f7e
dayul <dyulchoo@naver.com>
2024-01-15 16:20:49 +0900

    데이터 타입 예제

---
+++ javascript/datatype.js
@@ -0,0 +1,20 @@
+var name = '다율';      // String 타입
+var age = '17';         // Number 타입
+var flag = true;        // Boolean 타입
+var lists = [1, 2, 3];  // Object 타입
+var lists = {one: 1, two: 2, three: 3};
+
+var a = 5 + 10;
+console.log(a);     // 15
+
+var b = 5 + '10';
+console.log(b);     // 510
+
+var c = 5 + '명';
+console.log(c);     // 5명
+
+var d = true + 1;   // true : 1, false : 0
+console.log(d);     // 2
+
+var e = '자바 스크립트는 ' + '재미있다';    // 자바 스크립트는 재미있다
+console.log(e);
