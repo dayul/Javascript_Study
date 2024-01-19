4039f91baf9f5ffda225a2326f2e57a5f11b86e8
dayul <dyulchoo@naver.com>
2024-01-19 20:20:13 +0900

    데이터 타입 예제1

---
+++ javascript/datatype1.js
@@ -0,0 +1,14 @@
+console.log('edward', typeof 'edward');
+console.log('10', typeof 10);
+console.log('3.14', typeof 3.14);
+console.log('true', typeof true);
+console.log('mybox', typeof mybox);
+console.log('null', typeof null);
+console.log('nudefined', typeof undefined);
+console.log('[1, 2, 3]', typeof [1, 2, 3]);
+
+console.log(1/0);
+console.log('홍길동' * 3);
+console.log(isNaN(3));
+console.log(isNaN('3'));
+console.log(isNaN('홍길동'));
