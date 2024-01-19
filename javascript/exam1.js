08d73674bf82d75a91f2ff601482469bdaca9dc6
dayul <dyulchoo@naver.com>
2024-01-19 20:34:39 +0900

    exam1 예제 실습

---
+++ javascript/exam1.js
@@ -0,0 +1,8 @@
+var myBox;
+var yourBox = null;
+
+console.log(myBox, typeof myBox);
+console.log(yourBox, typeof yourBox);
+
+console.log('==', null == undefined);
+console.log('===', null === undefined);
