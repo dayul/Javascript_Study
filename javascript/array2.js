d301db11f25b93ce69fff059d2c5db73a95a794c
dayul <dyulchoo@naver.com>
2024-02-17 23:58:47 +0900

    배열의 속성과 메소드 알아보기 예제

---
+++ javascript/array2.js
@@ -0,0 +1,10 @@
+console.log('1. fruit.length');
+var fruit = ['사과', '키위', '망고'];
+console.log('fruit: ' + fruit);
+console.log('fruit.length: ' + fruit.length);   // 저장된 데이터의 개수(길이)를 반환
+
+console.log('2. fruit.sort');
+fruit = ['사과', '키위', '망고'];
+console.log('fruit (before): ' + fruit);
+console.log('fruit.sort: ' + fruit.sort());     // 한글은 가나다순(오름차순)으로 정렬
+console.log('fruit (after): ' + fruit);
