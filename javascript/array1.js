06731c886b0fe6ac74bdad76cd97d5ddc52b246f
dayul <dyulchoo@naver.com>
2024-02-17 23:39:03 +0900

    배열 생성 및 읽고 추가하기 예제

---
+++ javascript/array1.js
@@ -0,0 +1,14 @@
+var myArray = new Array(1, 2, '딸기', 1.47);
+
+var car = ['소형차', '대형차'];
+var fruit = ['딸기', '사과', '수박'];
+
+console.log('car 배열의 0번째 원소: ' + car[0]);
+
+// 원소 추가하기
+car[2] = '스포츠카';
+console.log('car 배열의 원소: ' + car.toString());
+
+// 원소 바꾸기
+car[1] = '버스';
+console.log('car 배열의 원소: ' + car.toString());
