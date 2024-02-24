4bd29f862e57c70b2b0b8ccb978a92dc84f9361a
dayul <dyulchoo@naver.com>
2024-02-24 23:07:08 +0900

    join, pop 함수 사용 예제

--- javascript/array2.js
+++ javascript/array2.js
@@ -8,3 +8,13 @@ fruit = ['사과', '키위', '망고'];
 console.log('fruit (before): ' + fruit);
 console.log('fruit.sort: ' + fruit.sort());     // 한글은 가나다순(오름차순)으로 정렬
 console.log('fruit (after): ' + fruit);
+
+console.log('3. fruit.join');
+console.log('fruit (before): ' + fruit);
+console.log('fruit.join: ' + fruit.join('-'));     // 모든 원소를 '-'문자로 연결
+console.log('fruit (after): ' + fruit);
+
+console.log('4. fruit.pop');
+console.log('fruit (before): ' + fruit);
+console.log('fruit.pop: ' + fruit.pop());     // 배열의 마지막 원소를 제거
+console.log('fruit (after): ' + fruit);
